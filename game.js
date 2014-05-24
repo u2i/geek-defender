71137b541b15b827a68489ef7ce63eb56e420946
Piotr Krawiec <piotr@krawiec.me>
2014-05-24 13:59:02 +0000

    Blank geek-defender template to clone to Cloud9

---
+++ game.js
@@ -0,0 +1,21 @@
+var game;
+
+window.onload = function() {
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-window');
+    game.state.add('playing', playingState);
+    game.state.start('playing');
+};
+
+var playingState = {
+    preload: function() {
+
+    },
+
+    create: function() {
+
+    },
+
+    update: function() {
+
+    }
+}
